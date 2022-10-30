05571d5fe675ef021da3a923cdbb82ab2ce81f1f
unknown <foong.wob.5@google.com>
2022-10-31 04:33:46 +0700

    P6_4_1204075

---
+++ P6_4_1204075/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace P6_4_1204075
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtHuruf = new System.Windows.Forms.TextBox();
+            this.numeric = new System.Windows.Forms.NumericUpDown();
+            this.txtAngka = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMatkul = new System.Windows.Forms.TextBox();
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epError = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtUAS = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtUTS = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtHuruf
+            // 
+            this.txtHuruf.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtHuruf.Location = new System.Drawing.Point(278, 67);
+            this.txtHuruf.Name = "txtHuruf";
+            this.txtHuruf.Size = new System.Drawing.Size(100, 20);
+            this.txtHuruf.TabIndex = 0;
+            this.txtHuruf.TextChanged += new System.EventHandler(this.txtHuruf_Leave);
+            this.txtHuruf.Leave += new System.EventHandler(this.txtHuruf_Leave);
+            // 
+            // numeric
+            // 
+            this.numeric.Location = new System.Drawing.Point(278, 221);
+            this.numeric.Name = "numeric";
+            this.numeric.Size = new System.Drawing.Size(57, 20);
+            this.numeric.TabIndex = 1;
+            // 
+            // txtAngka
+            // 
+            this.txtAngka.Location = new System.Drawing.Point(278, 120);
+            this.txtAngka.Name = "txtAngka";
+            this.txtAngka.Size = new System.Drawing.Size(100, 20);
+            this.txtAngka.TabIndex = 2;
+            this.txtAngka.TextChanged += new System.EventHandler(this.txtAngka_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "NPM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NAMA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(197, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Semester";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(197, 351);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Nilaii UTS";
+            // 
+            // txtMatkul
+            // 
+            this.txtMatkul.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtMatkul.Location = new System.Drawing.Point(278, 262);
+            this.txtMatkul.Name = "txtMatkul";
+            this.txtMatkul.Size = new System.Drawing.Size(120, 20);
+            this.txtMatkul.TabIndex = 9;
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // epError
+            // 
+            this.epError.ContainerControl = this;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Tampilkan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(278, 176);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 12;
+            this.txtEmail.Leave += new System.EventHandler(this.txtEmail_Leave);
+            // 
+            // txtUAS
+            // 
+            this.txtUAS.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtUAS.Location = new System.Drawing.Point(278, 301);
+            this.txtUAS.Name = "txtUAS";
+            this.txtUAS.Size = new System.Drawing.Size(120, 20);
+            this.txtUAS.TabIndex = 14;
+            this.txtUAS.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUAS_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(197, 308);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Nilaii UAS";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(197, 269);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Mata Kuliah";
+            // 
+            // txtUTS
+            // 
+            this.txtUTS.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtUTS.Location = new System.Drawing.Point(278, 344);
+            this.txtUTS.Name = "txtUTS";
+            this.txtUTS.Size = new System.Drawing.Size(120, 20);
+            this.txtUTS.TabIndex = 18;
+            this.txtUTS.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUTS_KeyPress);
+            this.txtUTS.Leave += new System.EventHandler(this.txtAkhir_Leave);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(325, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(215, 25);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Penilaian Mahasiswa";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtUTS);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtUAS);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMatkul);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAngka);
+            this.Controls.Add(this.numeric);
+            this.Controls.Add(this.txtHuruf);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHuruf;
+        private System.Windows.Forms.NumericUpDown numeric;
+        private System.Windows.Forms.TextBox txtAngka;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMatkul;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.ErrorProvider epError;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtUAS;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtUTS;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ P6_4_1204075/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace P6_4_1204075
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtHuruf_Leave(object sender, EventArgs e)
+        {
+            if ((txtHuruf.Text).All(Char.IsLetter))
+            {
+                epWarning.SetError(txtHuruf, "");
+                epCorrect.SetError(txtHuruf, "Betul!");
+                epError.SetError(txtHuruf, "");
+            }
+            else
+            {
+                epWarning.SetError(txtHuruf, "");
+                epCorrect.SetError(txtHuruf, "");
+                epError.SetError(txtHuruf, "Inputan hanya boleh huruf!");
+            }
+        }
+
+        private void txtAngka_TextChanged(object sender, EventArgs e)
+        {
+            if ((txtAngka.Text).All(Char.IsNumber))
+            {
+                epWarning.SetError(txtAngka, "");
+                epCorrect.SetError(txtAngka, "Betul!");
+                epError.SetError(txtAngka, "");
+            }
+            else
+            {
+                epWarning.SetError(txtAngka, "");
+                epCorrect.SetError(txtAngka, "");
+                epError.SetError(txtAngka, "Inputan hanya boleh angka!");
+            }
+        }
+
+        private void txtEmail_Leave(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(txtEmail.Text, @"^^[@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                epWarning.SetError(txtAngka, "");
+                epCorrect.SetError(txtAngka, "Betul!");
+                epError.SetError(txtAngka, "");
+            }
+            else
+            {
+                epWarning.SetError(txtAngka, "");
+                epCorrect.SetError(txtAngka, "");
+                epError.SetError(txtAngka, "Format email salah!\nContoh : a@b.c");
+            }
+        }
+
+        private void txtAkhir_Leave(string[] args)
+        {
+           
+        }
+
+        private void txtAkhir_Leave(object sender, EventArgs e)
+        {
+ 
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            double a, b, i;
+            double.TryParse(txtUTS.Text, out a);
+            double.TryParse(txtUAS.Text, out b);
+            i = a + b;
+            if (i >= 75)
+            {
+                MessageBox.Show("Nama : " + txtHuruf.Text +
+                "\nNPM : " + txtAngka.Text +
+                "\nEmail : " + txtEmail.Text +
+                "\nSemester : " + numeric.Text +
+                "\nMata Kuliah : " + txtMatkul.Text,
+                "Kamu Lulus Mata Kuliah ini");
+
+                txtUAS.Text = "";
+                txtUTS.Text = "";
+                txtUAS.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Nama : " + txtHuruf.Text +
+                "\nNPM : " + txtAngka.Text +
+                "\nEmail : " + txtEmail.Text +
+                "\nSemester : " + numeric.Text +
+                "\nMata Kuliah : " + txtMatkul.Text , 
+                "Kamu Mengulang Mata Kuliah ini");
+            }
+        }
+
+        private void txtUAS_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back;
+        }
+
+        private void txtUTS_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back;
+        }
+    }
+}
